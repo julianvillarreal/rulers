8a9b6298f46d8913c178a0e1254f6c744a963e73
Julian Villarreal <julian.villarreal@comunidadfeliz.cl>
2023-05-02 22:13:28 -0500

    exercise-1

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -5,6 +5,7 @@ require_relative "rulers/version"
 module Rulers
   class Application
     def call(env)
+      `echo debug > debug.txt`;
       [200, {'Content-Type' => 'text/html'},
         ["Hello from Ruby on Rulers!"]]
     end
