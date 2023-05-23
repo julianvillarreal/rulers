9233044527d41b62148123f203178ac7530dd168
Julian Villarreal <julian.villarreal@comunidadfeliz.cl>
2023-05-22 22:04:18 -0500

    chapter 2 - v6

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -11,6 +11,10 @@ module Rulers
         return [404, {'Content-Type' => 'text/html'}, []]
       end
 
+      if env['PATH_INFO'] == '/'
+        env['PATH_INFO'] = '/home/index'
+      end
+
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
       begin
