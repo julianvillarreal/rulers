fd6e7dbaabeef3a30c444a4be1df4e62bd87c958
Julian Villarreal <julian.villarreal@comunidadfeliz.cl>
2023-05-23 22:21:59 -0500

    chapter 3 CamelCase to snake_case

---
+++ lib/rulers/util.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# CamelCase to snake_case
+module Rulers
+  def self.to_underscore(string)
+    string.gsub(/::/, '/')
+          .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+          .gsub(/([a-z\d])([A-Z])/, '\1_\2')
+          .tr("-", "_")
+          .downcase
+  end
+end
