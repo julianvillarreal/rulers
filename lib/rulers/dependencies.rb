c50e7cd014d1de399cff9c386f59918e33618c62
Julian Villarreal <julian.villarreal@comunidadfeliz.cl>
2023-05-23 23:21:56 -0500

    chapter 3 exercise 1

--- lib/rulers/dependencies.rb
+++ lib/rulers/dependencies.rb
@@ -3,7 +3,14 @@
 # constant loading (classes)
 class Object
   def self.const_missing(c)
+    @calling_const_missing ||= {}
+    return nil if @calling_const_missing[c]
+
+    @calling_const_missing[c] = true
     require Rulers.to_underscore(c.to_s)
-    Object.const_get(c)
+    klass = Object.const_get(c)
+    @calling_const_missing[c] = false
+
+    klass
   end
 end
